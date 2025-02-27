220c56d7ed2ee594bf68b6c9d17b3fc736c2392f
HrolfDogface <david.eliason@gmail.com>
2025-02-27 09:11:41 -0800

    loop_max first checkin

---
+++ local/path/home/loop_max.ts
@@ -0,0 +1,41 @@
+/** @param {NS} ns */
+export async function main(ns: NS) {
+
+  //args[0]:executing host name
+  //args[1]:target host name
+
+  const executingServer: string = String(ns.args[0]);
+  const targetServer: string = String(ns.args[1]);
+  
+  ns.kill("security.js", executingServer, targetServer);
+  ns.kill("money.js", executingServer, targetServer);
+  ns.kill("hack.js", executingServer, targetServer);
+
+  const freeRam = ns.getServerMaxRam(executingServer) - ns.getServerUsedRam(executingServer);
+  
+  const weakenRam = ns.getScriptRam("security.js", executingServer);
+  
+  const growRam = ns.getScriptRam("money.js", executingServer);  
+  
+  const hackRam = ns.getScriptRam("hack.js", executingServer);
+  
+  const weakenThreadCount: number = Math.floor((freeRam - 5) * 0.17 / weakenRam);
+    
+  const growThreadCount: number = Math.floor((freeRam - 5) * 0.77 / growRam);
+    
+  const hackThreadCount: number = Math.floor((freeRam - 5) * 0.05 / hackRam);
+
+  ns.exec("security.js", executingServer, weakenThreadCount, targetServer);
+  while (ns.getServerSecurityLevel(targetServer) > (ns.getServerMinSecurityLevel(targetServer))){
+    await ns.sleep(10000);
+  }
+
+  ns.exec("money.js", executingServer, growThreadCount, targetServer);
+  while (ns.getServerMoneyAvailable(targetServer) < (ns.getServerMaxMoney(targetServer))){
+    await ns.sleep(10000);
+  }
+
+
+  ns.exec("hack.js", executingServer, hackThreadCount, targetServer);
+
+}
