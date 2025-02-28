1afd0f0b41627ab927523b28004c66a7d6bffd43
HrolfDogface <david.eliason@gmail.com>
2025-02-27 22:32:12 -0800

    changed max_money to typescript and updated todo

---
+++ local/path/home/max_money.ts
@@ -0,0 +1,8 @@
+/** @param {NS} ns */
+export async function main(ns: NS) {
+
+const hostName: string = String(ns.args[0]);
+
+const maxMoney: number = ns.getServerMaxMoney(hostName);
+ns.tprintf(`%s server has max $%d max money.`, hostName, maxMoney);
+}
