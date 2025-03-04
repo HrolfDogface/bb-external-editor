373c12ee7e4d3896315a9cfcb3e3e6c2cff11b13
HrolfDogface <david.eliason@gmail.com>
2025-03-04 09:53:04 -0800

    add number formatting

--- local/path/home/scan_all.ts
+++ local/path/home/scan_all.ts
@@ -9,14 +9,14 @@ export async function main(ns: NS) {
   ns.write("scan_all.txt", "\nsorted by level\n\n", "a");
   neighbors = neighbors.sort(function (a, b) { return b.level - a.level; });
   for (let i: number = 0; i < neighbors.length; i++) {
-    ns.write("scan_all.txt", neighbors[i].hostName + " " + neighbors[i].money + " " + neighbors[i].level + "\n", "a");
+    ns.write("scan_all.txt", neighbors[i].hostName + " $" + ns.formatNumber(neighbors[i].money) + " " + neighbors[i].level + "\n", "a");
 
   }
   neighbors = neighbors.sort(function (a, b) { return b.money - a.money; });
 
   ns.write("scan_all.txt", "\nsorted by money\n\n", "a");
   for (let i = 0; i < neighbors.length; i++) {
-    ns.write("scan_all.txt", neighbors[i].hostName + " " + neighbors[i].money + " " + neighbors[i].level + "\n", "a");
+    ns.write("scan_all.txt", neighbors[i].hostName + " $" + ns.formatNumber(neighbors[i].money) + " " + neighbors[i].level + "\n", "a");
   }
 
 }
@@ -32,7 +32,7 @@ export function search(ns: NS, hostName: string, maxLevel: number) {
     const money: number = ns.getServerMaxMoney(neighbor[i]);
     const level: number = ns.getServerRequiredHackingLevel(neighbor[i]);
     const files: string[] = ns.ls(neighbor[i]);
-    ns.write("scan_all.txt", neighbor[i] + " " + money + " " + level + " " + files.join(" ") + "\n", "a");
+    ns.write("scan_all.txt", neighbor[i] + " $" + ns.formatNumber(money) + " " + level + " " + files.join(" ") + "\n", "a");
     if (level < maxLevel) {
       neighborReturn.push(new ServerInfo(neighbor[i], money, level));
     }
