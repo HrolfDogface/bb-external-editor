7cdfec4d97b7d525265916f359801732b9aa10ee
HrolfDogface <david.eliason@gmail.com>
2025-03-05 15:53:59 -0800

    updates

--- local/path/home/server.js
+++ local/path/home/server.js
@@ -2,7 +2,7 @@
 export async function main(ns) {
 
   //const ram = 1024 * 1024;
-  const ram = 128;//ns.getPurchasedServerMaxRam();
+  const ram = 512;//ns.getPurchasedServerMaxRam();
   
   
   let cost = ns.getPurchasedServerCost(ram);
