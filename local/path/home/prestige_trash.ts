36cab2f9c22be424f0eb1868b83ca0fb7f8babd4
HrolfDogface <david.eliason@gmail.com>
2025-03-07 12:58:35 -0800

    update

--- local/path/home/prestige_trash.ts
+++ local/path/home/prestige_trash.ts
@@ -59,7 +59,7 @@ export async function main(ns: NS) {
     let scanLevel: number = level/3;
     if (servers.length > 10) scanLevel = level/2
     ns.exec("trash_bash.ts", "home", 1, scanLevel);
-    const serverCost = ns.getPurchasedServerCost(ns.getServerMaxRam(servers[0]);
+    const serverCost = ns.getPurchasedServerCost(ns.getServerMaxRam(servers[0]));
     while (true){
       if (ns.getHackingLevel() > level * 1.25){
         break;
