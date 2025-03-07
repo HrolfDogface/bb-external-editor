bbe67168d79d40d4c239930e04284a9f82dedea7
HrolfDogface <david.eliason@gmail.com>
2025-03-07 07:52:14 -0800

    lower hack target to 20% money

--- local/path/home/batch/batcher.ts
+++ local/path/home/batch/batcher.ts
@@ -67,8 +67,8 @@ export async function main(ns: NS) {
     const weaken2Delay: number = 60;
     const batchDelay: number = 120;
 
-    const hackThreads: number =  Math.floor(ns.hackAnalyzeThreads(targetHost, maxMoney/2));
-    const growthThreads: number = Math.ceil(ns.growthAnalyze(targetHost, 2.2));
+    const hackThreads: number =  Math.floor(ns.hackAnalyzeThreads(targetHost, maxMoney/5));
+    const growthThreads: number = Math.ceil(ns.growthAnalyze(targetHost, 1.3));
     const hackSecurity: number = ns.hackAnalyzeSecurity(hackThreads, targetHost);
     const growSecurity: number = growthThreads * 0.004;
 
