ec80d22368c89696bb753792d39a72a895082fbd
HrolfDogface <david.eliason@gmail.com>
2025-03-07 11:00:56 -0800

    Revert "change badding to 2msec"
    This reverts commit a6501861858071cdb8cc05453cf2032ef59c7a7c.

--- local/path/home/batch/batcher.ts
+++ local/path/home/batch/batcher.ts
@@ -62,11 +62,11 @@ export async function main(ns: NS) {
     ns.write("batch/batchLog.txt", Date.now() + "[batcher.ts]: " + targetHost + " Hack time is "
             + hackTime + " and Grow time is " + growTime + " and Weaken time is " + weakenTime + "\n", "a");
 
-    const hackDelay: number = weakenTime - hackTime - 2;
+    const hackDelay: number = weakenTime - hackTime - 5;
     const weaken1Delay: number = 0;
-    const growDelay: number = weakenTime - growTime + 2;
-    const weaken2Delay: number = 4;
-    const batchDelay: number = 8;
+    const growDelay: number = weakenTime - growTime + 5;
+    const weaken2Delay: number = 10;
+    const batchDelay: number = 20;
 
     let hackThreads: number =  Math.floor(ns.hackAnalyzeThreads(targetHost, maxMoney/5));
     let growthThreads: number = Math.ceil(ns.growthAnalyze(targetHost, 1.3));
@@ -136,10 +136,10 @@ export async function main(ns: NS) {
           ns.kill(hackPids[i]);
         }
       }
-      hackPids.push(ns.exec("batch/H_worker.js", exHost, hackThreads, targetHost, hackTime, Date.now() + hackTime + hackDelay + 2));
-      ns.exec("batch/W_worker.js", exHost, weaken1Threads, targetHost, weakenTime, Date.now() + weakenTime + weaken1Delay + 2);
-      ns.exec("batch/G_worker.js", exHost, growthThreads, targetHost, growTime, Date.now() + growTime + growDelay + 2);    
-      ns.exec("batch/W_worker2.js", exHost, weaken2Threads, targetHost, weakenTime, Date.now() + weakenTime + weaken2Delay + 2, ns.pid, workerPid);
+      hackPids.push(ns.exec("batch/H_worker.js", exHost, hackThreads, targetHost, hackTime, Date.now() + hackTime + hackDelay + 5));
+      ns.exec("batch/W_worker.js", exHost, weaken1Threads, targetHost, weakenTime, Date.now() + weakenTime + weaken1Delay + 5);
+      ns.exec("batch/G_worker.js", exHost, growthThreads, targetHost, growTime, Date.now() + growTime + growDelay + 5);    
+      ns.exec("batch/W_worker2.js", exHost, weaken2Threads, targetHost, weakenTime, Date.now() + weakenTime + weaken2Delay + 5, ns.pid, workerPid);
       
       if (level != ns.getHackingLevel()){
         level = ns.getHackingLevel()
