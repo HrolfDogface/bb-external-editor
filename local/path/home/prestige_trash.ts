46195d804d4e4085a3b468e004ad3ac002e08f4d
HrolfDogface <david.eliason@gmail.com>
2025-03-07 22:11:23 -0800

    works with lots of servers

--- local/path/home/prestige_trash.ts
+++ local/path/home/prestige_trash.ts
@@ -60,15 +60,15 @@ export async function main(ns: NS) {
     let scanLevel: number = level/3;
     if (servers.length > 10) scanLevel = level/2;
     ns.exec("trash_bash.ts", "home", 1, scanLevel);
-   // const currentRam: number = ns.getServerMaxRam(servers[0]);
-    //const serverCost = ns.getPurchasedServerCost(currentRam);
+    const currentRam: number = ns.getServerMaxRam(servers[0]);
+    const serverCost = ns.getPurchasedServerCost(currentRam);
     while (true){
       if (ns.getHackingLevel() > level * 1.25){
         break;
       }
-     // if ((servers.length < 10)&&(ns.getServerMoneyAvailable("home") > serverCost)){
-     //   break;
-     // }
+      if ((servers.length < 10)&&(ns.getServerMoneyAvailable("home") > serverCost)){
+        break;
+      }
       await ns.sleep(10000);
     }
 
