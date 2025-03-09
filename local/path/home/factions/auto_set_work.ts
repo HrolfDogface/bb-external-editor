0059de1906f6ce3459a0d5bd345208ad969ffda6
HrolfDogface <david.eliason@gmail.com>
2025-03-09 09:19:07 -0700

    reorder factions

--- local/path/home/factions/auto_set_work.ts
+++ local/path/home/factions/auto_set_work.ts
@@ -1,6 +1,6 @@
 export async function main(ns: NS) {
 
-    const factions: string[] = ["CyberSec", "Aevum", "Chongqing", "Daedalus", "Ishima", "New Tokyo", "NiteSec", "Sector-12", "Slum Snakes", "Tetrads", "The Black Hand", "Tian Di Hui", "Volhaven"];
+    const factions: string[] = ["CyberSec", "Daedalus", "NiteSec", "The Black Hand", "Aevum", "Chongqing", "Ishima", "New Tokyo", "Sector-12", "Slum Snakes", "Tetrads", "Tian Di Hui", "Volhaven"];
 
     for(let i = 0; i < factions.length; i++){
         if ((ns.singularity.getFactionRep(factions[i])>0) && ((ns.singularity.getFactionFavor(factions[i]) + ns.singularity.getFactionFavorGain(factions[i])) < 150)){
