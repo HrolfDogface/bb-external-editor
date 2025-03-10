8e16529e9b67821211936e61768d02797dbf9de5
HrolfDogface <david.eliason@gmail.com>
2025-03-10 16:06:21 -0700

    get nfg buy loop working

--- local/path/home/proto_installer.ts
+++ local/path/home/proto_installer.ts
@@ -7,12 +7,12 @@ export async function main(ns: NS) {
         ramUpgradeCost = 2 * ramUpgradeCost * (1024 * 8) / currentRam;
     }
 
-    const nfgBaseCost = ns.singularity.getAugmentationPrice("NeuroFlux Governor");
-    const nfgCost = nfgBaseCost * Math.pow(1.14, 10) * Math.pow(1.9, 10);
+    const nfgBaseCost: number = ns.singularity.getAugmentationPrice("NeuroFlux Governor");
+    const nfgCost: number = nfgBaseCost * Math.pow(1.14, 10) * Math.pow(1.9, 10);
 
-    const installCost = ramUpgradeCost + nfgCost;
+    const installCost: number = ramUpgradeCost + nfgCost;
 
-    //ns.tprint("Money threshold for buying and installing augments: " + ns.formatNumber(installCost));
+   // ns.tprint("Money threshold for buying and installing augments: " + ns.formatNumber(installCost));
 
     if (ns.getServerMoneyAvailable("home") > installCost){
         ns.singularity.upgradeHomeRam();
@@ -21,6 +21,29 @@ export async function main(ns: NS) {
         }
 
         //Purchase NFG
+        while(ns.getServerMoneyAvailable("home") > ns.singularity.getAugmentationPrice("NeuroFlux Governor")){
+            const nfgRepReq: number = ns.singularity.getAugmentationRepReq("NeuroFlux Governor");
+            const csecRep: number = ns.singularity.getFactionRep("CyberSec");
+            if(nfgRepReq > csecRep){
+                if(ns.getFavorToDonate() > ns.singularity.getFactionFavor("CyberSec")){
+                    break;
+                }else{
+                    ns.singularity.donateToFaction("CyberSec", 100);
+                    const donationResult: number = ns.singularity.getFactionRep("CyberSec") - csecRep;
+                    const donationAmmount: number = 99 * (nfgRepReq - csecRep) / donationResult;
+                    if(donationAmmount > ns.singularity.getFactionFavor("CyberSec")){
+                        break;
+                    }else {
+                        ns.singularity.donateToFaction("CyberSec", donationAmmount);
+                    }
+                }
+            }
+            ns.singularity.purchaseAugmentation("CyberSec", "NeuroFlux Governor");
+        }
+
+       
+
+        
         //Purchase other augments
         //purchase cores
         //install
