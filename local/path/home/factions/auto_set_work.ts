38d15dd6b202686bf7196b91a447d2629c426282
HrolfDogface <david.eliason@gmail.com>
2025-03-10 11:42:19 -0700

    remove sector 12 and slum snakes from the auto work script

--- local/path/home/factions/auto_set_work.ts
+++ local/path/home/factions/auto_set_work.ts
@@ -1,6 +1,7 @@
 export async function main(ns: NS) {
 
-    const factions: string[] = ["CyberSec", "Daedalus", "NiteSec", "The Black Hand", "Aevum", "Chongqing", "Ishima", "New Tokyo", "Sector-12", "Slum Snakes", "Tetrads", "Tian Di Hui", "Volhaven"];
+    //const factions: string[] = ["CyberSec", "Daedalus", "NiteSec", "The Black Hand", "Aevum", "Chongqing", "Ishima", "New Tokyo", "Sector-12", "Slum Snakes", "Tetrads", "Tian Di Hui", "Volhaven"];
+    const factions: string[] = ["CyberSec", "Daedalus", "NiteSec", "The Black Hand", "Aevum", "Chongqing", "Ishima", "New Tokyo", "Tetrads", "Tian Di Hui", "Volhaven"];
 
     for(let i = 0; i < factions.length; i++){
         if ((ns.singularity.getFactionRep(factions[i])>0) && ((ns.singularity.getFactionFavor(factions[i]) + ns.singularity.getFactionFavorGain(factions[i])) < 150)){
