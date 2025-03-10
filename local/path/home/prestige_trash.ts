b8e7cd31ef71e395a20ff3187ac108bedb45ef5d
HrolfDogface <david.eliason@gmail.com>
2025-03-10 16:05:26 -0700

    increase loop time to 5 minutes

--- local/path/home/prestige_trash.ts
+++ local/path/home/prestige_trash.ts
@@ -95,7 +95,7 @@ export async function main(ns: NS) {
       ns.exec("pop_all.ts", "home", 1);
       await ns.sleep(60000);
       ns.exec("factions/join_all_factions.ts", "home", 1);
-      await ns.sleep(10000);
+      await ns.sleep(300000);
       ns.exec("factions/auto_set_work.ts", "home", 1);
     }
 
