85a72dc1567d7a67e77ba674a3a6fc4f737b8d33
HrolfDogface <david.eliason@gmail.com>
2025-03-11 07:31:03 -0700

    update to favor required to donate function

--- local/path/home/factions/auto_set_work.ts
+++ local/path/home/factions/auto_set_work.ts
@@ -4,7 +4,7 @@ export async function main(ns: NS) {
     const factions: string[] = ["CyberSec", "Daedalus", "NiteSec", "The Black Hand", "Aevum", "Chongqing", "Ishima", "New Tokyo", "Tetrads", "Tian Di Hui", "Volhaven"];
 
     for(let i = 0; i < factions.length; i++){
-        if ((ns.singularity.getFactionRep(factions[i])>0) && ((ns.singularity.getFactionFavor(factions[i]) + ns.singularity.getFactionFavorGain(factions[i])) < 150)){
+        if ((ns.singularity.getFactionRep(factions[i])>0) && ((ns.singularity.getFactionFavor(factions[i]) + ns.singularity.getFactionFavorGain(factions[i])) < ns.getFavorToDonate())){
             ns.singularity.workForFaction(factions[i],ns.singularity.getFactionWorkTypes(factions[i])[0]);
             break;
         }
