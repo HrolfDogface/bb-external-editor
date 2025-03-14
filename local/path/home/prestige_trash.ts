29e57765d6f10eb8598dc1c3cf747fc0a12a690d
HrolfDogface <david.eliason@gmail.com>
2025-03-14 01:02:44 -0700

    updates

--- local/path/home/prestige_trash.ts
+++ local/path/home/prestige_trash.ts
@@ -100,6 +100,7 @@ export async function main(ns: NS) {
       ns.exec("factions/join_all_factions.ts", "home", 1);
       await ns.sleep(300000);
       ns.exec("factions/auto_set_work.ts", "home", 1);
+      ns.exec("proto_installer.ts", "home", 1);
     }
 
 
--- local/path/home/proto_installer.ts
+++ local/path/home/proto_installer.ts
@@ -15,9 +15,11 @@ export async function main(ns: NS) {
     }
 
     if (ns.getServerMaxRam("home") < (1024 * 8)) return;
-    
+
+    let nfgCount: number = 10 - ns.singularity.getOwnedAugmentations(true).length - ns.singularity.getOwnedAugmentations(false).length;
+    if (nfgCount < 0) nfgCount = 0;
     const nfgBaseCost: number = ns.singularity.getAugmentationPrice("NeuroFlux Governor");
-    const nfgCost: number = nfgBaseCost * Math.pow(1.14, 10) * Math.pow(1.9, 10);
+    const nfgCost: number = nfgBaseCost * Math.pow(1.14, nfgCount) * Math.pow(1.9, nfgCount);
 
     //const installCost: number = ramUpgradeCost + nfgCost;
 
@@ -53,16 +55,55 @@ export async function main(ns: NS) {
 
             //go through each faction to see which ones have any rep to detect joined factions
         for(let i = 0; i < Object.keys(ns.enums.FactionName).length; i++){
-            if (ns.singularity.getFactionRep(Object.values(ns.enums.FactionName)[i]) > 0){
-                ns.tprint(ns.enums.FactionName[i]);
-                const augmentations: string [] = ns.singularity.getAugmentationsFromFaction(Object.values(ns.enums.FactionName)[i]);
+            const faction: string = Object.values(ns.enums.FactionName)[i];
+            const rep: number = ns.singularity.getFactionRep(faction)
+            if (rep > 0){
+                //ns.tprint(faction);
+                
+                const ownedAugs: string [] = ns.singularity.getOwnedAugmentations(true);
+                const augmentations: string [] = ns.singularity.getAugmentationsFromFaction(faction);
                 for (let i = 0; i<augmentations.length;i++){
-                    ns.tprint(augmentations[i]);
+                    //ns.tprint(augmentations[i]);
+                    //ns.tprint(ns.singularity.getAugmentationPrice(augmentations[i]));
+
+                    if (ownedAugs.includes(augmentations[i])){
+                        continue;
+                    }
+                    if (ns.singularity.getAugmentationPrice(augmentations[i]) > ns.getServerMoneyAvailable("home")){
+                        continue;
+                    }
+                    //need to check if donation is possible to buy augmentation
+                    if (rep > ns.singularity.getAugmentationRepReq(augmentations[i])){
+                        ns.singularity.purchaseAugmentation(faction, augmentations[i]);
+                    }else if(ns.getFavorToDonate() <= ns.singularity.getFactionFavor(faction)){
+                        ns.singularity.donateToFaction(faction, 100);
+                        const donationResult: number = ns.singularity.getFactionRep(faction) - rep;
+                        const donationAmmount: number = 99 * (ns.singularity.getAugmentationRepReq(augmentations[i]) - rep) / donationResult;
+                        if(donationAmmount <= ns.getServerMoneyAvailable("home")){
+                           ns.singularity.donateToFaction(faction, donationAmmount);
+                           ns.singularity.purchaseAugmentation(faction, augmentations[i]);
+                        }
+                    }
+
+                    
                 }
             }
+        }
 
+        if (ns.singularity.getOwnedAugmentations(true).length - ns.singularity.getOwnedAugmentations(false).length >= 10){
+            while (ns.getServerMoneyAvailable("home") > ns.singularity.getUpgradeHomeRamCost()){
+                ns.singularity.upgradeHomeRam();
+            }
+
+            while (ns.getServerMoneyAvailable("home") > ns.singularity.getUpgradeHomeCoresCost()){
+                ns.singularity.upgradeHomeCores();
+            }
+
+            ns.singularity.installAugmentations("prestige_trash.ts")
 
         }
+
+
         //Purchase other augments
             //go through each faction's augments and find the most expensive one that can be afforded across all factions
             //buy the most expensive augment then the next until can't afford any more or they are all purchased
