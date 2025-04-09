6728b1e46b32004a81af38ee4f072c32c2462837
HrolfDogface <david.eliason@gmail.com>
2025-04-09 11:40:27 -0700

    first check in

---
+++ local/path/home/port_enum.ts
@@ -0,0 +1,18 @@
+export enum PortNumber {
+    phase = 5,
+    travelFlag = 11,
+    autoWorkFlag = 12,
+    sleeveTask = 13,
+    destroyBitnodeFlag = 14,
+}
+
+export enum Phase {
+    murderParty,
+    buyRam,
+    gangStartup1, //recruit bangers
+    gangStartup2, //buy banger augments
+    gangGang, //buy slum snakes augments
+    stackNFG,
+    daedalusRep, 
+    stackNFG2, // destroy world demon if destruction is enabled, may need to stack more NFG to get to needed level
+}
